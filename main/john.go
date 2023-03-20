ded63ece227b93d5a133d2eafd80c3bc3b2cf2c1
h1x <h1x@linuxconsole.net>
2023-03-20 20:41:49 +0100

    add a function to check if zip is encrypted

---
+++ main/john.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"time"
+
+	str "strings"
+)
+
+func zipIsEnc(file string) (bool, error) {
+	cmd := exec.Command("unzip", "-t", file)
+
+	var stdErr bytes.Buffer
+	cmd.Stderr = &stdErr
+
+	err := cmd.Start()
+	if err != nil {
+		return false, err
+	}
+
+	time.Sleep(1 * time.Second)
+
+	err = cmd.Process.Kill()
+	if err != nil {
+		return false, err
+	}
+
+	line, _, _ := str.Cut(stdErr.String(), "\n")
+	begin := "[" + file + "] "
+	if str.HasPrefix(line, begin) && str.HasSuffix(line, "password: ") {
+		return true, nil
+	}
+
+	return false, nil
+}
